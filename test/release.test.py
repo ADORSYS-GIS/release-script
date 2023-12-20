b7714d7fa51fa96528a21475f6d4676b792f7038
Stephane Segning <selastlambou@gmail.com>
2023-12-20 11:05:13 +0100

    feat(global): added basic python code

---
+++ test/release.test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestRelease(unittest.TestCase):
+    def test_release(self):
+        self.assertEqual(1, 1)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
