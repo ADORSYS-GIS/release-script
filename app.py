2a3b8a9784df82a05bebcf5b6b7d10ce752f04fd
Stephane SEGNING LAMBOU <selastlambou@gmail.com>
2023-12-21 19:51:08 +0100

    feat(python): added typer to handle command line interactions (#41)

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
-import src.realease as r
+from src.realease import app as release_app
+from typer import Typer
+
+global_app = Typer()
+
+# https://typer.tiangolo.com/tutorial/subcommands/nested-subcommands/
+# Adding a subcommand to a subcommand is done by calling add_typer() on the parent app.
+global_app.add_typer(release_app, name="release")
+
+@global_app.command()
+def say_hello(to: str) -> None:
+    """
+    Say hello to TO, optionally with a
+
+    Args:
+        to (str): The person to say hello to
+    """
+    print(f"Hello {to}")
 
 if __name__ == '__main__':
-    print(r.create_release)
+    global_app()
--- src/realease.py
+++ src/realease.py
@@ -1,2 +1,27 @@
-def create_release():
+from typer import Typer
+
+app = Typer()
+
+
+@app.command()
+def create(
+    version: str,
+    description: str = "",
+    branch: str = "",
+    commit: str = ""
+):
+    """
+    Create a new release.
+
+    Args:
+        version (str): Version of the release.
+        description (str, optional): Description message of the release.
+        branch (str, optional): Branch name of the release.
+        commit (str, optional): Commit associated to the release.
+    """
+    pass
+
+
+@app.command()
+def revert(version: str, branch: str):
     pass
